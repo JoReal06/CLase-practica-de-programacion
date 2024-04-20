fca89ba6fc7515369b3222be7936ebd1af3d1ab5
jose <joreal06@gmail.com>
2024-04-19 21:36:36 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btn_director = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(125, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 0;
+            // 
+            // btn_director
+            // 
+            btn_director.Location = new Point(77, 24);
+            btn_director.Name = "btn_director";
+            btn_director.Size = new Size(146, 51);
+            btn_director.TabIndex = 1;
+            btn_director.Text = "Seleccionar archivos";
+            btn_director.UseVisualStyleBackColor = true;
+            btn_director.Click += btn_director_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(312, 113);
+            Controls.Add(btn_director);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btn_director;
+    }
+}
---
+++ Ejercicio 1/Form1.cs
@@ -0,0 +1,17 @@
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_director_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog director = new OpenFileDialog();
+
+
+        }
+    }
+}
